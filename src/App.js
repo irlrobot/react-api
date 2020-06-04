0eddc886b8c7987182866b5387ede9235587599c
Josh Campbell <josh@p4a.net>
2020-06-03 20:12:08 -0700

    playing around with static data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React, {Component} from 'react';
 class App extends Component {
   render () {
     return (
-      // todo
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Josh Campbell</h5>
+          <h6 class="card-subtitle mb-2 text-muted">josh@p4a.net</h6>
+          <p class="card-text">420 blaze up</p>
+        </div>
+      </div>
     );
   }
 }
