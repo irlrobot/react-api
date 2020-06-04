d783afedaa9691372b74099bebe9d4eac7da0ae1
Josh Campbell <josh@p4a.net>
2020-06-03 20:55:23 -0700

    Updates

---
+++ precache-manifest.27eeefd17f919af1be2b1f95d99841d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511c3e5263f6a9e36174cb26f690fd34",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "8d6111ebf685a928e73d",
+    "url": "/react-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf08b996f2c53bc0fe94",
+    "url": "/react-api/static/js/2.0349b25f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-api/static/js/2.0349b25f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6111ebf685a928e73d",
+    "url": "/react-api/static/js/main.e793b8de.chunk.js"
+  },
+  {
+    "revision": "d83d0d6431466fa269cf",
+    "url": "/react-api/static/js/runtime-main.e5f5147a.js"
+  }
+]);
